4c2b295d1a6e656f3920af4afd7af4cc204624f8
Rob Louie T. Nicolas <roblouie47@gmail.com>
2025-11-15 23:06:46 +0800

    Updated admin section

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './tokens.css';
 import './index.css';
+import './tshirt.css';
 import App from './components/App';
 
 createRoot(document.getElementById('root')).render(<App />);
